0d32df32d4fa0529b9b8e0b3796fe1111ffd22fb
1n5omn1ac <59324018+1n5omn1ac@users.noreply.github.com>
2022-12-03 16:14:01 -0500

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,59 @@
+//Define variables
+let inputField;
+let submitButton;
+let isIndex;
+let wordAfterIs;
+
+//Function to get the text content of the webpage
+const getPageTextContent = () => {
+  let pageContent = document.body.innerText;
+  return pageContent;
+};
+
+//Function to find the index of the word 'is' in the page content using indexof
+const findIsIndex = (pageContent) => {
+  isIndex = pageContent.indexOf('is');
+  return isIndex;
+};
+
+//Function to get the word after 'is' in the page content + 10 spaces
+const getWordAfterIs = (pageContent, isIndex) => {
+  wordAfterIs = pageContent.substring(isIndex + 3, isIndex + 10);
+  return wordAfterIs;
+};
+
+//Function to get the input field on the webpage. Using query selector
+const getInputField = () => {
+  inputField = document.querySelector('input[type="text"]');
+  return inputField;
+};
+
+//Function to get the submit button on the webpage. change value/type as is depending on page.
+const getSubmitButton = () => {
+  submitButton = document.querySelector('input[value="submit"]');
+  return submitButton;
+};
+
+//Function to run the bot
+const runBot = () => {
+  //Get the page content
+  let pageContent = getPageTextContent();
+
+  //Find the index of 'is' in the page content
+  let isIndex = findIsIndex(pageContent);
+
+  //Get the word after 'is' in the page content
+  let wordAfterIs = getWordAfterIs(pageContent, isIndex);
+
+  //Get the input field
+  let inputField = getInputField();
+
+  //Get the submit button
+  let submitButton = getSubmitButton();
+
+  //Input the word after 'is' in the input field
+  inputField.value = wordAfterIs;
+
+  //Submit the form
+  submitButton.click();
+};
